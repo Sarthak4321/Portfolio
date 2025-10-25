5be03a85aa29f07cdd11189ab0049dbf5c30fe44
Sarthak4321 <roysarthak16@gmail.com>
2025-10-25 22:24:08 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,3 @@
-// const express = require('express');
-// const app = express();
-// app.use(express.static('distribution'));
-// const PORT = 3000;
-// const http = require('http');
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// require('dotenv').config();
-
-// // adding bodyparser
-// const bodyParser = require("body-parser");
-// app.use(bodyParser.json());
-
-// const db = require('./db');
-// const Massage = require('./model/massage');
-
-// app.set("view engine", "ejs");
-
-
-// app.use(express.json());
-
-
-// app.get('/', (req, res) => {
-//   res.render("index");
-// })
-
-
-// app.post('/massage', async (req, res) => {
-//   try {
-//     const userMassage = req.body;
-//     const massage = new Massage(userMassage);
-//     const savedDeta = await massage.save();
-//     // console.log("massage saved", savedDeta);
-//     res.status(200).render("massagesend");
-
-//   } catch (err) {
-//     res.status(400).render("massageSendUnsuccesful");
-//     // console.log("failed", err);
-
-//   }
-// })
-
-// app.listen(PORT, () => {
-//   console.log("✅Sarthsk's Server has connected succesfully");
-// });
-
-
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -114,6 +67,18 @@ app.post('/massage', async (req, res) => {
     </div>
   `
     });
+
+
+
+
+
+    if (res.ok) {
+        alert("✅ Password sent to your email!");
+        setEmail("");
+        onClose();
+      } else {
+        alert("❌ Failed to send email. Try again.");
+      }
 
 
     // 2. Notify admin
